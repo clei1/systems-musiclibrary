4f868857bdfb7a6169decb930b4f3def3b79f857
clei1 <clei1@stuy.edu>
2017-10-22 18:22:11 -0400

    still debugging :|

--- music_library.c
+++ music_library.c
@@ -31,7 +31,7 @@ struct song_node * find(char* artist, char* name, struct song_node* library[]){
 
 void clear_library(struct song_node* library[]){
   for(int i = 0; i < 26; i ++){
-    free_list(library[i]));
+    free_list(library[i]);
   }
 }
 
@@ -39,19 +39,35 @@ void print_artist(char* artist, struct song_node* library[]){
   print_songs(find_lib_artist(artist, library));
 }
 
-/*
-void shuffle(){
+struct song_node* shuffle(struct song_node* library[]){
   srand(time(NULL));
-  for(int i = 0; i < 26; i++){
-    
+  int count = rand() % 26;
+  while(library[count]){
+    count = rand() % 26;
   }
+  return random_song(library[count]);
 }
-*/
+
+void insert_song(struct song_node* library[], char* artist, char* name){
+  int count = artist[0] - 'a';
+  if(! library[count]){
+    struct song_node * new_song = (struct song_node *) malloc(sizeof(struct song_node));
+    strcpy(new_song -> artist, artist);
+    strcpy(new_song -> name, name);
+    new_song -> next = NULL;
+    library[count] = new_song;
+  }
+  else{
+    library[count] = insert(library[count], artist, name);
+  }
+  //return library[count];
+}
 
 int main(){
   printf("====================================\n\nMUSIC LIBRARY TESTS\n\n====================================\n");
   printf("Testing print_library\n");
 
+  /*
   struct song_node* a = (struct song_node *) malloc(sizeof(struct song_node));
   strcpy(a -> artist, "ac/dc");
   strcpy(a -> name, "thunderstruck");
@@ -59,6 +75,7 @@ int main(){
   struct song_node* p = (struct song_node *) malloc(sizeof(struct song_node));
   strcpy(p -> artist, "pearl jam");
   strcpy(p -> name, "alive");
+  */
   
   struct song_node * table[27];
   int alpha = 0;
@@ -69,9 +86,9 @@ int main(){
   table[26] = 0;
 
   print_library(table);
-
-  
-  
+  insert_song(table, "ac/dc", "thunderstruck");
+  insert_song(table, "pearl jam", "alive");
+  print_library(table);
 
   return 0;
 }
